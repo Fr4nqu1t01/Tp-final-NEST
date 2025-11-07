bf0cc37a58ab9e9358a23b6879ecc59c96f5ccbe
Fr4nqu1t01 <villalbafranco482@gmail.com>
2025-11-07 14:46:25 -0300

    modifico los nombres de los controladores, servicios y models para que coincidan entre si

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DueñosController } from './dueños/dueños.controller';
-import { DueñosService } from './dueños/dueños.service';
-import { DueñosModel } from './dueños/dueños.model';
+import { DueniosController } from './duenios/duenios.controller';
+import { DueniosService } from './duenios/duenios.service';
+import { DueniosModule } from './duenios/duenios.model';
 import { MascotasController } from './mascotas/mascotas.controller';
 import { MascotasService } from './mascotas/mascotas.service';
-import { MascotasModel } from './mascotas/mascotas.model';
+import { MascotasModule } from './mascotas/mascotas.model';
+import { TurnosController } from './turnos/turnos.controller';
+import { TurnosService } from './turnos/turnos.service';
+import { TurnosModule } from './turnos/turnos.model';
+import { HistorialController } from './historial/historial.controller';
+import { HistorialService } from './historial/historial.service';
+import { HistorialModule } from './historial/historial.module';
 
 @Module({
-  imports: [DueñosModel, MascotasModel],
-  controllers: [AppController, DueñosController, MascotasController],
-  providers: [AppService, DueñosService, MascotasService],
+  imports: [DueniosModule, MascotasModule, TurnosModule, HistorialModule],
+  controllers: [AppController, DueniosController, MascotasController, TurnosController, HistorialController],
+  providers: [AppService, DueniosService, MascotasService, TurnosService, HistorialService],
 })
 export class AppModule {}
